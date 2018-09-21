357ff532cddbc59726a306011b8da3db740c6dc5
George G. Vega Yon <g.vegayon@gmail.com>
2018-09-21 16:36:13 -0700

    Playing with exact ergms

--- models/powersets.r
+++ models/powersets.r
@@ -14,4 +14,33 @@ powerset <- function(n) {
   
 }
 
-ans <- powerset(4)
+Rcpp::sourceCpp("models/powersets.cpp")
+sets <- powerset(4)
+nets <- lapply(sets, function(s) {
+  
+  ans <- matrix(0, ncol=4, nrow=4)
+  ans[s+1] <- 1
+  ans
+})
+
+library(parallel)
+library(sna)
+library(ergm)
+
+stats <- mclapply(nets, function(n) {
+  summary(n ~ edges + balance + mutual)
+}, mc.cores = 4)
+
+S <- do.call(rbind, stats)
+
+set.seed(1123)
+net    <- sna::rgraph(4, tprob = .7)
+stats0 <- summary(net ~ edges + balance + mutual)
+ll <- function(params) {
+  
+  P <- matrix(params, ncol=3, nrow=nrow(S), byrow = TRUE)
+  sum(stats0*params) - sum(P * S)
+  
+}
+
+sol <- ABCoptim::abc_optim(c(.1, .1, .1), ll, fnscale=-1)
