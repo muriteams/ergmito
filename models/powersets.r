72b2e5aa26dc8b7584d142d5825ef4a6ce725aa1
George G. Vega Yon <g.vegayon@gmail.com>
2018-09-21 23:46:57 -0700

    Exact likelihood

--- models/powersets.r
+++ models/powersets.r
@@ -15,32 +15,35 @@ powerset <- function(n) {
 }
 
 Rcpp::sourceCpp("models/powersets.cpp")
-sets <- powerset(4)
-nets <- lapply(sets, function(s) {
-  
-  ans <- matrix(0, ncol=4, nrow=4)
-  ans[s+1] <- 1
-  ans
-})
+
+n <- 4
+sets <- powerset(n)
 
 library(parallel)
 library(sna)
 library(ergm)
 
-stats <- mclapply(nets, function(n) {
-  summary(n ~ edges + balance + mutual)
-}, mc.cores = 4)
+stats <- parallel::mclapply(sets, function(s) {
+  
+  ans <- matrix(0, ncol=n, nrow=n)
+  ans[s+1] <- 1
+  summary(ans ~ edges + meandeg + mutual)
+})
 
 S <- do.call(rbind, stats)
 
 set.seed(1123)
-net    <- sna::rgraph(4, tprob = .7)
-stats0 <- summary(net ~ edges + balance + mutual)
+net    <- sna::rgraph(n, tprob = .4)
+stats0 <- summary(net ~ edges + meandeg + mutual)
 ll <- function(params) {
   
   P <- matrix(params, ncol=3, nrow=nrow(S), byrow = TRUE)
-  sum(stats0*params) - sum(P * S)
+  sum(stats0*params) - log(sum(exp(P * S)))
   
 }
 
-sol <- ABCoptim::abc_optim(c(.1, .1, .1), ll, fnscale=-1)
+sol  <- optim(rnorm(3), ll, control=list(fnscale=-1, reltol=1e-25, maxit=2e3));sol
+sol2 <- ergm(net ~ edges + meandeg + mutual);summary(sol2)
+logLik(sol2)
+sol
+ll(sol2$coef)
