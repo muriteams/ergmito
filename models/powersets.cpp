9b674f21e25165e53bfcd02eaf548b3ada67e091
George G. Vega Yon <g.vegayon@gmail.com>
2018-09-23 23:20:58 -0700

    Almost getting the ermg specification

--- models/powersets.cpp
+++ models/powersets.cpp
@@ -2,10 +2,28 @@
 using namespace Rcpp;
 
 // [[Rcpp::export]]
+std::vector< int > make_sets(int n) {
+  
+  int m = n*(n-1);
+  std::vector< int > ans(m);
+  
+  int pos=-1, k=0;
+  for (int i=0; i<n; i++)
+    for (int j=0; j<n; j++) {
+      if (i!=j)
+        ans[++pos] = k;
+      ++k;
+    }
+    
+    return ans;
+}
+
+// [[Rcpp::export]]
 std::vector< std::vector<int> > powerset(int n) {
   
   int m = n*(n-1);
-  std::vector< std::vector< int > > sets(pow(2, m));
+  std::vector< int > set = make_sets(n);
+  std::vector< std::vector< int > > sets(pow(2.0, m));
   std::vector< int > v(1);
   
   int j = 0,k;
@@ -13,12 +31,14 @@ std::vector< std::vector<int> > powerset(int n) {
     k = j;
     for (int s = 0; s < k; s++) {
       sets.at(j) = sets.at(s);
-      sets.at(j++).push_back(i);
+      sets.at(j++).push_back(set[i]);
     }
-    v.at(0) = i;
+    v.at(0) = set[i];
     sets.at(j++) = v;
   }
 
   return sets;
   
 }
+
+
--- models/powersets.r
+++ models/powersets.r
@@ -16,34 +16,53 @@ powerset <- function(n) {
 
 Rcpp::sourceCpp("models/powersets.cpp")
 
-n <- 4
+n     <- 3
+npars <- 2
+
 sets <- powerset(n)
 
 library(parallel)
 library(sna)
 library(ergm)
 
+
+
 stats <- parallel::mclapply(sets, function(s) {
   
   ans <- matrix(0, ncol=n, nrow=n)
   ans[s+1] <- 1
-  summary(ans ~ edges + meandeg + mutual)
+  summary(ans ~ mutual + edges)
 })
 
 S <- do.call(rbind, stats)
+Smeans <- colMeans(S)["edges"]
+S <- S - matrix(Smeans, nrow = nrow(S), ncol=ncol(S), byrow = TRUE)
 
-set.seed(1123)
-net    <- sna::rgraph(n, tprob = .4)
-stats0 <- summary(net ~ edges + meandeg + mutual)
+set.seed(1155)
+net    <- as.matrix(netdiffuseR::rgraph_er(n, p = .75))
+stats0 <- summary(net ~ mutual + edges) - Smeans
 ll <- function(params) {
   
-  P <- matrix(params, ncol=3, nrow=nrow(S), byrow = TRUE)
+  P <- matrix(params, ncol=length(params), nrow=nrow(S), byrow = TRUE)
   sum(stats0*params) - log(sum(exp(P * S)))
   
 }
 
-sol  <- optim(rnorm(3), ll, control=list(fnscale=-1, reltol=1e-25, maxit=2e3));sol
-sol2 <- ergm(net ~ edges + meandeg + mutual);summary(sol2)
-logLik(sol2)
-sol
+gr <- function(params) {
+  
+  P <- matrix(params, ncol=length(params), nrow=nrow(S), byrow = TRUE)
+  sum(stats0) - 1/sum(exp(P * S))*sum(exp(P * S)*S)
+  
+}
+
+sol  <- optim(par = rnorm(npars), fn = ll, gr = gr, control=list(fnscale=-1, reltol=1e-25, maxit=2e3));sol
+sol2 <- ergm(net ~ mutual + edges, control =control.ergm(MCMC.samplesize = 2e3,MCMC.interval = 2e3));summary(sol2) 
 ll(sol2$coef)
+sol$par
+ll(sol$par)
+sol$par;sqrt(diag(-solve(optimHess(sol$par, ll))))
+
+ergm.exact(sol2$coef, sol2$formula)
+ergm.exact(sol$par, sol2$formula)
+ergm.allstats(sol2$formula)
+unique(S)
