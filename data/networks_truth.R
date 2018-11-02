b060ba9315ed7af47f15b134393961c713a35e8e
George G. Vega Yon <g.vegayon@gmail.com>
2018-11-02 12:47:56 -0700

    Working on network data

--- data/networks_truth.R
+++ data/networks_truth.R
@@ -7,7 +7,12 @@ library(magrittr)
 
 #read in data of participant perceptions of advice ties among their other teammates 
 
-IDS     <- c(LETTERS[1:4], NA, "E")
+IDS   <- c(LETTERS[1:4], NA, "E")
+IDS_E <- c(LETTERS[1:3], NA, "D")
+
+groups_sizes <- readr::read_csv("data-raw/Study1_Group sizes.csv") %>%
+  rename(group = Group, n = groupSize)
+
 
 edgelist_pre <- haven::read_sav("data-raw/MURI_Survey_1.sav") %>%
 
@@ -29,7 +34,7 @@ edgelist_pre <- haven::read_sav("data-raw/MURI_Survey_1.sav") %>%
       ego == "B" ~ IDS[-2][alter],
       ego == "C" ~ IDS[-3][alter],
       ego == "D" ~ IDS[-4][alter],
-      ego == "E" ~ IDS[-5][alter],
+      ego == "E" ~ IDS_E[alter],
       TRUE ~ "ERROR"
     ),
     survey = 1L,
@@ -56,14 +61,58 @@ edgelist_post <- haven::read_sav("data-raw/MURI_Survey_3.sav") %>%
       ego == "B" ~ IDS[-2][alter],
       ego == "C" ~ IDS[-3][alter],
       ego == "D" ~ IDS[-4][alter],
-      ego == "E" ~ IDS[-5][alter],
+      ego == "E" ~ IDS_E[alter],
       TRUE ~ "ERROR"
     ),
     survey = 3L,
     network = stringr::str_extract(Code, "(?<=[_])[0-9]+")
   ) 
 
-edgelist <- bind_rows(edgelist_post, edgelist_pre)
+edgelists <- bind_rows(edgelist_post, edgelist_pre) %>%
+  # Merging group size. We use full to make sure that we have all the data
+  full_join(groups_sizes)
 
-saveRDS(edgelist, "data/edgelist.rds")
+# saveRDS(edgelist, "data/edgelist_truth.rds")
 
+# Now, creating the adjmat
+networks <- vector("list", 2)
+names(networks) <- c(1, 3)
+
+for (s in c(1, 3)) {
+  
+  networks[[as.character(s)]] <- edgelists %>%
+    
+    filter(survey == s) %>%
+    
+    split(.$group) %>%
+    lapply(function(g) {
+      
+      # Group ids
+      n   <- g$n[1]
+      ids <- LETTERS[1:n]
+      
+      # Looping at the group level
+      M <- matrix(0, ncol = n, nrow = n, dimnames = list(ids, ids))
+      M <- replicate(n, M, simplify = FALSE)
+      names(M) <- ids
+      
+      # Keep complete cases
+      g <- g[complete.cases(g),]
+      
+      if (nrow(g) == 0)
+        return(M)
+      
+      message("Group ", g$group[1], "survey ", s, " done.")
+      
+      # Adding 1s
+      for (i in 1:nrow(g))
+        M[[g$viewer[i]]][g$ego[i], g$alter[i]] <- 1L
+      
+      M
+      
+    })
+  
+}
+
+
+# saveRDS(networks, "data/networks_truth.rds")
