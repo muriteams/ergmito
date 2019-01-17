510a1bcc21b64d1f9964f9b9ad55fe8538a32670
George G. Vega Yon <g.vegayon@gmail.com>
2019-01-17 13:17:33 -0800

    mc.cores fixed to 1

--- tests/testthat/test-new_rlergm.R
+++ tests/testthat/test-new_rlergm.R
@@ -6,9 +6,9 @@ test_that("Using the count_stats vs ergm::summary_formula is equal", {
   nets <- rbernoulli(c(4,3,4))
   
   Sys.setenv(ERGMITO_TEST = "")
-  set.seed(1);ans0 <- new_rergmito(nets ~ edges + mutual, sizes = 2:3, mc.cores = 2L)
+  set.seed(1);ans0 <- new_rergmito(nets ~ edges + mutual, sizes = 2:3, mc.cores = 1L)
   Sys.setenv(LERGM_TEST = 1)
-  set.seed(1);ans1 <- new_rergmito(nets ~ edges + mutual, sizes = 2:3, mc.cores = 2L)
+  set.seed(1);ans1 <- new_rergmito(nets ~ edges + mutual, sizes = 2:3, mc.cores = 1L)
   
   expect_equal(ans0$prob, ans1$prob)
   
