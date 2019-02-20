435ca3124500bdad7e03bda39202b802b71a7af2
George G. Vega Yon <g.vegayon@gmail.com>
2019-02-19 16:26:35 -0800

    Adding stat counts for faster sampling

--- R/count_stats.R
+++ R/count_stats.R
@@ -25,12 +25,107 @@
 #' # Comparing
 #' all.equal(unname(ans0), unname(ans1))
 #' 
-count_stats <- function(X, terms, attrs = NULL) {
+count_stats <- function(X, ...) UseMethod("count_stats")
+
+#' Parses a formula to the model parameters (and attributes if applicable)
+#' @param x A formula.
+#' @noRd
+analyze_formula <- function(x, check_w_ergm = TRUE) {
+  
+  # Getting the parameters
+  terms_passed <- attr(stats::terms(x), "term.labels")
+  
+  # Do these have attributes
+  has_attr <- grepl("^[a-zA-Z0-9]+[(].+[)]", terms_passed)
+  
+  # Capturing attributes
+  terms_attrs <- vector("list", length(terms_passed))
+  for (term in which(has_attr)) {
+    terms_attrs[[term]] <- gsub("\"?[)].*", "", gsub(".+[(]\"?", "", terms_passed[term]))
+  }
+  
+  terms_names <- gsub("[(].+", "", terms_passed)
+  
+  # Do the terms exists?
+  terms_exists <- sapply(terms_names, function(z) {
+    out <- capture.output(ergm::search.ergmTerms(name = z))[1]
+    !grepl("^No terms named", out, ignore.case = TRUE)
+    })
+  
+  if (any(!terms_exists))
+    stop("The following terms are not found in `ergm`: ", 
+         paste(terms_names[!terms_exists], sep = ", "), ".", call. = FALSE)
+  
+  list(
+    passed    = terms_passed,
+    names     = terms_names,
+    attrnames = terms_attrs,
+    attrs     = vector("list", length(terms_names))
+  )
+  
+  
+}
+
+#' @export
+#' @rdname count_stats
+count_stats.formula <- function(X, ...) {
+  
+  # Retrieving networks
+  LHS <- eval(X[[2]], envir = environment(X))
+  if (network::is.network(LHS))
+    LHS <- list(LHS)
+  
+  # Analyzing the formula
+  ergm_terms <- analyze_formula(X)
+  
+  # # Can we do it?
+  # available <- which(!(ergm_terms$names %in% count_available()))
+  # if (length(available)) 
+  #   stop("The following terms cannot be computed")
+  
+  # Capturing attributes
+  for (a in seq_along(ergm_terms$attrnames)) {
+    ergm_terms$attrs[[a]] <- if (is.null(ergm_terms$attrnames[[a]]))
+      numeric(0)
+    else
+      lapply(LHS, function(net) {
+        network::get.vertex.attribute(net, attrname = ergm_terms$attrnames[[a]])
+      })
+  }
+  
+  # Coercing into the appropiate type
+  if (network::is.network(LHS))
+    LHS <- list(network::as.matrix(LHS))
+  else if (is.list(LHS)) {
+    is_net <- sapply(LHS, network::is.network)
+    LHS[is_net] <- lapply(LHS[is_net], as.matrix)
+  }
+  
+  out <- matrix(nrow = nnets(LHS), ncol = length(ergm_terms$names),
+                dimnames = list(NULL, ergm_terms$passed))
+  for (j in 1:ncol(out)) {
+    
+    out[, j] <- count_stats(
+      X = LHS,
+      ergm_terms$names[j],
+      ergm_terms$attrs[[j]]
+      )
+    
+  }
+  
+  out
+
+  
+}
+
+#' @export
+#' @rdname count_stats
+count_stats.list <- function(X, terms, attrs = NULL) {
   
   chunks <- make_chunks(length(X), 2e5)
   
   if (!length(attrs))
-    attrs <- rep_len(numeric(0), length(X))
+    attrs <- replicate(length(X), numeric(0), simplify = FALSE)
   
   ans <- matrix(NA, nrow = length(X), ncol=length(terms))
   
